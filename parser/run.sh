0420eef4c3da9a75b2caebcd365c74e3e6a8fac7
Max Malysh <someone307@gmail.com>
2015-10-29 03:19:29 +0300

    New tests

--- parser/run.sh
+++ parser/run.sh
@@ -1,2 +1,6 @@
-# Add a line for antlr here
-python3 main.py tests/1.txt
+# Just a temporary solution for testing
+for i in `seq 0 16`;
+do
+    echo tests/$i.txt
+    python3 main.py tests/$i.txt
+done
