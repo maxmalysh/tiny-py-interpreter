52eb8b2c245e18ca9ad9096baef91fc4752ca48b
maxmalysh <maxmalysh@gmail.com>
2015-11-15 21:48:56 +0300

    Finally working python-like shell. Produces AST for any input.

--- tinypy/parser/Errors.py
+++ tinypy/parser/Errors.py
@@ -1,9 +1,12 @@
-from antlr4.error.Errors import RecognitionException, ParseCancellationException
+from antlr4.error.ErrorListener import ErrorListener, ProxyErrorListener
+from antlr4.error.Errors import RecognitionException, ParseCancellationException, InputMismatchException
 from antlr4.error.Errors import CancellationException
 from antlr4.error.ErrorStrategy import BailErrorStrategy, DefaultErrorStrategy
 from antlr4.Parser import Parser
 from antlr4.Token import Token
-from TinyPyLexer import TinyPyLexer
+import sys
+
+from TinyPyParser import TinyPyParser
 
 
 class IndentationErr(RecognitionException):
@@ -11,14 +14,24 @@ class IndentationErr(RecognitionException):
         super().__init__(message="unindent does not match any outer indentation level")
         self.line = line
 
-class SingleInputUnfinished(Exception):
+class SingleInputUnfinished(RecognitionException):
     def __init__(self):
         super().__init__()
 
 class CustomErrorStrategy(DefaultErrorStrategy):
+
+    def __init__(self):
+        super().__init__()
+
+    # def recoverInline(self, recognizer:Parser):
+    #     self.errors_encountered += 1
+    #     super().recoverInline(recognizer)
+
     def reportError(self, recognizer:Parser, e:RecognitionException):
         if isinstance(e, IndentationErr):
             self.reportIndendationError(recognizer, e)
+        elif isinstance(e, SingleInputUnfinished):
+            super().reportError(recognizer, e)
         else:
             super().reportError(recognizer, e)
 
@@ -28,3 +41,48 @@ class CustomErrorStrategy(DefaultErrorStrategy):
         offendingToken.column = 0
         recognizer.notifyErrorListeners(e.message, offendingToken, e)
         raise ParseCancellationException(e)
+
+
+
+class CustomErrorListener(ProxyErrorListener):
+
+    def __init__(self):
+        super().__init__(delegates=[])
+        self.errors_encountered = 0
+        self.input_unfinished = False
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.errors_encountered += 1
+
+        #
+        # User just started compound statement (e.g. "if True:\n")
+        #
+        if offendingSymbol.type == Token.EOF:
+            if isinstance(e, InputMismatchException):
+                if isinstance(e.ctx, TinyPyParser.SuiteContext):
+                    self.input_unfinished = True
+
+        #
+        # This happens when user have not entered the second newline in compound statement
+        # TODO: handle this by using parser state (based on a rule context)
+        #
+        if offendingSymbol.type == Token.EOF and msg == "missing <INVALID> at '<EOF>'":
+            self.input_unfinished = True
+
+        super().syntaxError(recognizer, offendingSymbol, line, column, msg, e)
+
+    def addDelegatee(self, delegatee):
+        self.delegates.append(delegatee)
+
+
+class BufferedErrorListener(ErrorListener):
+
+    def __init__(self):
+        self.buffer = ""
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.buffer += ("line " + str(line) + ":" + str(column) + " " + msg) + '\n'
+
+    def printBuffer(self):
+        print(self.buffer)
+
--- tinypy/parser/tinypy.py
+++ tinypy/parser/tinypy.py
@@ -3,9 +3,10 @@ from pprint import pprint
 
 
 import antlr4
+from antlr4.error.ErrorListener import ConsoleErrorListener
 from antlr4.tree.Trees import Trees
 import AST
-from Errors import CustomErrorStrategy
+from Errors import CustomErrorStrategy, CustomErrorListener, BufferedErrorListener
 import Utils
 
 from CustomLexer import CustomLexer
@@ -14,10 +15,10 @@ from TinyPyParser import TinyPyParser
 
 
 class InteractiveShell:
-    greeting = "Ctrl + D to feed the input\n" + \
-               "Ctrl + C to stop the loop\n"
+    greeting =  "Press Ctrl + C to exit\n"
 
     def __init__(self):
+        self.readMore = False
         pass
 
 
@@ -31,54 +32,93 @@ class InteractiveShell:
         self.parser._errHandler = CustomErrorStrategy()
 
     def loop(self):
-        #while True:
+        while True:
             try:
-                single_input = sys.stdin.read()
+                if self.readMore:
+                    sys.stdout.write("... ")
+                    sys.stdout.flush()
+                    single_input += sys.stdin.readline()
+                else:
+                    sys.stdout.write(">>> ")
+                    sys.stdout.flush()
+                    single_input = sys.stdin.readline()
 
-
-                print("We have read the following line: [" + single_input.replace('\n', '\\n') + "]")
+                #print("We have read the following text: [" + single_input.replace('\n', '\\n') + "]")
                 #print("Codepoints: ", [ord(c) for c in single_input])
 
-
                 input_stream = antlr4.InputStream(single_input)
-                #input_stream.data.append(-1)
 
-
-                # Instantiate an run generated lexer
+                # Instantiate and run generated lexer
                 self.lexer = CustomLexer(input_stream)
                 self.tokens = antlr4.CommonTokenStream(self.lexer)
 
 
-                # Instantiate and run generated parser
+                # Setting up error handling stuff
+                error_handler = CustomErrorStrategy()
+                error_listener = CustomErrorListener()
+                buffered_errors = BufferedErrorListener()
+                error_listener.addDelegatee(buffered_errors)
+
                 self.parser = TinyPyParser(self.tokens)
-                self.parser._errHandler = CustomErrorStrategy()
+                self.parser._errHandler = error_handler
 
+                self.parser.removeErrorListeners()
+                self.parser.addErrorListener(error_listener)
 
+                # Parse input
                 parse_tree = self.parser.single_input()
 
-                print("Tokens we got so far...")
-                indents = dedents = newline = 0
-                for token in self.tokens.tokens:
-                    print(AST.nameFor(token.type))
-                    if   token.type == TinyPyParser.INDENT: indents  += 1
-                    elif token.type == TinyPyParser.DEDENT: dedents  += 1
-                    elif token.type == TinyPyParser.NEWLINE: newline += 1
+                # Just debug info
+                #self.checkTokens(error_listener)
 
-                print("%d | %d | %d" %(indents, dedents, newline))
 
-                # Simple statement should end with 0 indents, 0 dedents, 1 newline and then EOF
-                # 1-line compound statement should end with NEWLINE, NEWLINE, EOF with 0 indents / dedents
-                # multiline compound statement have 1+ indents/dedents of the same amount and DEDENT-NEWLINE-EOF
+                if error_listener.input_unfinished:
+                    # User has not finished his input yet, read the next line and repeat
+                    self.readMore = True
+                    continue
+                elif error_listener.errors_encountered > 0:
+                    # Errors encountered, start over
+                    print(buffered_errors.buffer)
+                    self.readMore = False
+                    continue
+                else:
+                    # Successfully parsed the input, next time start over
+                    self.readMore = False
 
+                # Let's build an AST now...
+                ast = AST.AST(tree=parse_tree)
+                print(ast)
 
                 # String, containing parse tree representation
-                full_parse_tree = Trees.toStringTree(parse_tree, recog=self.parser)
+                #print(Trees.toStringTree(parse_tree, recog=self.parser))
 
-                ast = AST.AST(tree=parse_tree)
-                print(ast.__str__(True))
-                #print(full_parse_tree)
+                # Evaluate it...
+                # ...
+
             except antlr4.RecognitionException as e:
                 print("Caught" + str(e) )
+
+
+    def checkTokens(self, error_listener):
+        print("Tokens we got so far...")
+        indents = dedents = newline = eofs = 0
+        for token in self.tokens.tokens:
+            print(AST.nameFor(token.type))
+            if token.type == TinyPyParser.INDENT:
+                indents += 1
+            elif token.type == TinyPyParser.DEDENT:
+                dedents += 1
+            elif token.type == TinyPyParser.NEWLINE:
+                newline += 1
+            elif token.type == TinyPyParser.EOF:
+                eofs += 1
+
+        # Simple statement should end with 0 indents, 0 dedents, 1 newline and then EOF
+        # 1-line compound statement should end with NEWLINE, NEWLINE, EOF with 0 indents / dedents
+        # multiline compound statement have 1+ indents/dedents of the same amount and DEDENT-NEWLINE-EOF
+        print("%d | %d | %d | %d" % (indents, dedents, newline, eofs))
+        print("Got %d errors and %d" % (error_listener.errors_encountered, error_listener.input_unfinished))
+
 
     def print_greeting(self):
         print(self.greeting)
