200cf8fdb0cd06cda465f6ac66df686fe31439cf
maxmalysh <maxmalysh@gmail.com>
2015-12-20 18:45:01 +0300

    Improved automated unit tests

--- tinypy/run_tests.py
+++ tinypy/run_tests.py
@@ -17,64 +17,70 @@ tinypy_binary = "python3 " + os.getcwd() + "/tinypy.py"
 python_binary = sys.executable
 
 
-class ParserTests(unittest.TestCase):
+class FileTestCase(unittest.TestCase):
 
-    def test_proper_syntax(self):
-        for file in os.listdir(proper_tests_dir):
-            if not file.endswith(".txt"):
-                continue
-
-            with self.subTest(msg=file):
-                file_path = proper_tests_dir + '/' + file
-
-                result = subprocess.run(
-                    tinypy_binary + " --parse " + file_path, stdout=PIPE, stderr=STDOUT,
-                    shell=True, universal_newlines=True
-                )
-
-                # self.assertEqual(result.returncode, 0)
-                self.assertTrue(not result.stdout)
-                self.assertTrue(not result.stderr)
-
-    def test_fails(self):
-        for file in os.listdir(failing_tests_dir):
-            if not file.endswith(".txt"):
-                continue
-
-            with self.subTest(msg="fail/" + file):
-                file_path = failing_tests_dir + '/' + file
-
-                result = subprocess.run(
-                    tinypy_binary + " --parse " + file_path, stdout=PIPE, stderr=STDOUT,
-                    shell=True, universal_newlines=True
-                )
-
-                # self.assertNotEqual(result.returncode, 0)
-                self.assertTrue(result.stdout != '')
+    def __init__(self, file_path):
+        super().__init__()
+        self.file_path = file_path
 
 
-class SemanticTests(unittest.TestCase):
+class ProperParserTest(FileTestCase):
 
-    def test_semantics(self):
-        for file in os.listdir(proper_tests_dir):
-            if not file.endswith(".py"):
-                continue
+    def runTest(self):
+        result = subprocess.run(
+            tinypy_binary + " --parse " + self.file_path, stdout=PIPE, stderr=STDOUT,
+            shell=True, universal_newlines=True
+        )
 
-            with self.subTest(msg=file):
-                file_path = proper_tests_dir + '/' + file
+        # self.assertEqual(result.returncode, 0)
+        self.assertTrue(not result.stdout)
+        self.assertTrue(not result.stderr)
 
-                tinypy_result = subprocess.run(
-                    tinypy_binary + ' ' + file_path, stdout=PIPE, stderr=STDOUT,
-                    shell=True, universal_newlines=True
-                )
-                python_result = subprocess.run(
-                    python_binary + ' ' + file_path, stdout=PIPE, stderr=STDOUT,
-                    shell=True, universal_newlines=True
-                )
 
-                # self.assertEqual(result.returncode, 0)
-                self.assertEqual(tinypy_result.stdout, python_result.stdout)
+class FailingParserTest(FileTestCase):
+
+    def runTest(self):
+        result = subprocess.run(
+            tinypy_binary + " --parse " + self.file_path, stdout=PIPE, stderr=STDOUT,
+            shell=True, universal_newlines=True
+        )
+
+        # self.assertNotEqual(result.returncode, 0)
+        self.assertTrue(result.stdout != '')
+
+
+class SemanticTest(FileTestCase):
+
+    def runTest(self):
+        tinypy_result = subprocess.run(
+            tinypy_binary + ' ' + self.file_path, stdout=PIPE, stderr=STDOUT,
+            shell=True, universal_newlines=True
+        )
+        python_result = subprocess.run(
+            python_binary + ' ' + self.file_path, stdout=PIPE, stderr=STDOUT,
+            shell=True, universal_newlines=True
+        )
+
+        # self.assertEqual(result.returncode, 0)
+        self.assertEqual(tinypy_result.stdout, python_result.stdout)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestSuite()
+
+    for file in os.listdir(proper_tests_dir):
+
+        if file.endswith(".txt"):
+            file_path = proper_tests_dir + '/' + file
+            suite.addTest(ProperParserTest(file_path))
+
+        elif file.endswith(".py"):
+            file_path = proper_tests_dir + '/' + file
+            suite.addTest(SemanticTest(file_path))
+
+    for file in os.listdir(failing_tests_dir):
+        if file.endswith(".txt"):
+            file_path = failing_tests_dir + '/' + file
+            suite.addTest(FailingParserTest(file_path))
+
+    unittest.TextTestRunner().run(suite)
